c2b418f72e44c63d86fd9bd7425cad36d98c1189
xiaomo1q <55676359+xiaomo1q@users.noreply.github.com>
2022-08-20 17:59:00 +0800

    fix: 接口测试

--- ComboBackEnd/app/controller/home.js
+++ ComboBackEnd/app/controller/home.js
@@ -3,10 +3,10 @@
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
-  async index() {
-    const { ctx } = this;
-    ctx.body = 'hi, egg';
-  }
+    async index() {
+        const { ctx, app } = this;
+        ctx.body = await app.mysql.get('book_detail', { book_type: '热门图书' });
+    }
 }
 
 module.exports = HomeController;
--- ComboBackEnd/config/plugin.js
+++ ComboBackEnd/config/plugin.js
@@ -2,30 +2,33 @@
 
 /** @type Egg.EggPlugin */
 module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
+    // had enabled by egg
+    // static: {
+    //   enable: true,
+    // }
 
-  // 跨域
-  cors: {
-    enable: true,
-    package: 'egg-cors',
-  },
-
-  // mongodb 数据库
-  mongoose: {
-    enable: true, // 开启插件
-    package: 'egg-mongoose',
-  },
-  //  鉴权
-  jwt: {
-    enable: true,
-    package: 'egg-jwt',
-  },
-  // websocket
-  io: {
-    enable: true,
-    package: 'egg-socket.io',
-  },
+    // 跨域
+    cors: {
+        enable: true,
+        package: 'egg-cors',
+    },
+    mysql: {
+        enable: true,
+        package: 'egg-mysql',
+    },
+    // mongodb 数据库
+    mongoose: {
+        enable: true, // 开启插件
+        package: 'egg-mongoose',
+    },
+    //  鉴权
+    jwt: {
+        enable: true,
+        package: 'egg-jwt',
+    },
+    // websocket
+    io: {
+        enable: true,
+        package: 'egg-socket.io',
+    },
 };
