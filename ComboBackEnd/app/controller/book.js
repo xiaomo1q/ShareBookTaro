5c9af9f78b4a5e1358726d20159603a3609904eb
xiaomo1q <55676359+xiaomo1q@users.noreply.github.com>
2022-09-26 18:02:01 +0800

    fix: del

--- ComboBackEnd/app/controller/book.js
+++ ComboBackEnd/app/controller/book.js
@@ -3,28 +3,31 @@
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
-    async add_book() {
-        const { ctx, app } = this;
-        const user = ctx.request.body;
-        const result = await service.user.create(user);
-        console.log(result);
-        ctx.body = result;
-        if (result.affectedRows === 1) {
-            ctx.body = {
-                code: 0,
-                data: result
-            }
-        } else {
-            ctx.body = {
-                code: 1,
-                data: '用户添加失败'
-            }
-        }
-    }
+    /** 添加图书 */
+    // async add_book() {
+    //     const { ctx, app } = this;
+    //     const user = ctx.request.body;
+    //     const result = await service.user.create(user);
+    //     console.log(result);
+    //     ctx.body = result;
+    //     if (result.affectedRows === 1) {
+    //         ctx.body = {
+    //             code: 0,
+    //             data: result
+    //         }
+    //     } else {
+    //         ctx.body = {
+    //             code: 1,
+    //             data: '添加失败'
+    //         }
+    //     }
+    // }
+    /** 获取图书分类  */
     async get_book_type() {
         const { ctx, app } = this;
         ctx.body = await app.mysql.select('book_type');
     }
+    /** 根据分类图书列表 */
     async get_book_list() {
         const { ctx, app } = this;
         const { title } = ctx.request.query;
@@ -37,6 +40,7 @@ class HomeController extends Controller {
             ctx.body = { code: 1, data: '类型必填' }
         }
     }
+    /** 获取单本图书信息 */
     async get_only_book_detail() {
         const { ctx, app } = this;
         const { isbn } = ctx.request.query;
